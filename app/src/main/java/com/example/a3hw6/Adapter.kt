6c29aa4e4ec5551d67b63e1493b7743326693c3d
Askar999 <askartemirbekov190@gmail.com>
2022-04-29 17:04:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a3hw6/Adapter.kt
@@ -0,0 +1,25 @@
+package com.example.a3hw6
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter : RecyclerView.Adapter<Adapter.MyViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
